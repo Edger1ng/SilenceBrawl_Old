b8185140e5545ae646f8169355af70f9a9f009a6
roman <roman@romandeMacBook-Air.local>
2024-11-24 20:37:13 +0100

    В projectamber будет работа над v30 #projectamber

--- Supercell.Laser.Server/Message/MessageManager.cs
+++ Supercell.Laser.Server/Message/MessageManager.cs
@@ -105,7 +105,7 @@ namespace Supercell.Laser.Server.Message
             
             LobbyInfoMessage b = new()
             {
-                LobbyData = $"SilenceBrawl\nTG: @silencebrawl \n1.3.1\n{abd}Players Online: {Sessions.Count}\nДата: {DateTime.UtcNow}.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+                LobbyData = $"SilenceBrawl\nTG: @silencebrawl \n1.3.1\n{abd}Players Online: {Sessions.Count}\nДата: {DateTime.UtcNow}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
                 PlayersCount = 0
             };
             Connection.Send(b);
